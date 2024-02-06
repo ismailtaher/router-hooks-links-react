de2a221bc369a14079c37e789533e3080c7b4710
ismailtaher <ismail.taher@ymail.com>
2024-02-06 18:27:03 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import { Route, Routes, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Nav></Nav>
+      <Routes>
+        <Route exact path="/" element={<Home></Home>}></Route>
+        <Route exact path="/post" element={<NewPost></NewPost>}></Route>
+        <Route path="/post/:id" element={<PostPage></PostPage>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<Missing></Missing>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
